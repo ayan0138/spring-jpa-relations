443189b34af63bd75e05ed4c790dcda3e010b90d
ayan0138 <ayah0002@stud.ek.dk>
2025-09-01 12:36:42 +0200

    Completed exercise 1.7

--- src/main/java/ek/osnb/jpa/orders/repository/OrderRepository.java
+++ src/main/java/ek/osnb/jpa/orders/repository/OrderRepository.java
@@ -1,4 +1,9 @@
 package ek.osnb.jpa.orders.repository;
 
-public interface OrderRepository {
+import ek.osnb.jpa.orders.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
 }
--- src/main/java/ek/osnb/jpa/orders/service/OrderServiceImpl.java
+++ src/main/java/ek/osnb/jpa/orders/service/OrderServiceImpl.java
@@ -1,5 +1,48 @@
 package ek.osnb.jpa.orders.service;
 
-public class OrderServiceImpl {
+import ek.osnb.jpa.orders.model.Order;
+import ek.osnb.jpa.orders.model.OrderStatus;
+import ek.osnb.jpa.orders.repository.OrderLineRepository;
+import ek.osnb.jpa.orders.repository.OrderRepository;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+
+private final OrderRepository orderRepository;
+private final OrderLineRepository orderLineRepository;
+
+
+public OrderServiceImpl(OrderRepository orderRepository, OrderLineRepository orderLineRepository) {
+    this.orderRepository = orderRepository;
+    this.orderLineRepository = orderLineRepository;
 }
+
+    @Override
+    public List<Order> getAllOrders(OrderStatus status) {
+        return List.of();
+    }
+
+    @Override
+    public Order getOrderById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Order createOrder(Order order) {
+        return null;
+    }
+
+    @Override
+    public Order updateOrder(Long id, Order order) {
+        return null;
+    }
+
+    @Override
+    public void deleteOrder(Long id) {
+
+    }
+}
+
